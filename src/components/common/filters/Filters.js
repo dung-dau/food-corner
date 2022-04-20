4f39e54f2d8ec7e73ff77a15eb287afd9bc24617
Dung Dau <dudau0924222@gmail.com>
2022-04-19 23:21:30 -0400

    finish tab options and filter options

---
+++ src/components/common/filters/Filters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FilterItem from './filterItem/FilterItem';
+import './filters.css';
+
+function Filters({filterList}) {
+  return (
+    <div className='filters'>
+      {filterList && filterList.map((filter) => {
+        return <FilterItem filter={filter} key={filter.id}/>
+      })}
+    </div>
+  )
+}
+
+export default Filters
--- src/components/delivery/Delivery.js
+++ src/components/delivery/Delivery.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import Filters from '../common/filters/Filters';
 import './delivery.css';
 
 function Delivery() {
+  const deliveryFilters = [
+      {
+          id: 1,
+          icon: <i class="fi fi-rr-settings-sliders absolute-center"></i>,
+          title: "Filters",
+      },
+      {
+        id: 2,
+        title: "Rating: 4.0+",
+      },
+      {
+        id: 3,
+        title: "Safe and Hygienic",
+      },
+      {
+        id: 4,
+        title: "Pure Veg",
+      },
+      {
+        id: 5,
+        title: "Delivery Time",
+        icon: <i className="fi fi-rr-apps-sort absolute-center"></i>,
+      },
+      {
+        id: 6,
+        title: "Great Offers",
+      },
+  ]
   return (
-    <div>Delivery</div>
+    <div className='max-width'>
+        <Filters filterList={deliveryFilters}/>
+    </div>
   )
 }
 
